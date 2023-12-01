f259edaa6d473d4256da4736121dcd070248bb8b
Tomas Gilys <tomas.gilys@pm.me>
2023-12-01 18:40:49 +0200

    Add 1st day solution

--- 2023/day01a/src/main.rs
+++ 2023/day01a/src/main.rs
@@ -2,5 +2,28 @@ use std::fs;
 
 fn main() {
     let file = fs::read_to_string("./input.txt").unwrap();
-    println!("{file}");
+    let mut res: i32 = 0;
+    for row in file.lines() {
+        let mut row_res : String = "".to_string();
+
+        for char in row.chars() {
+            if char.is_digit(10) {
+                row_res.push(char);
+                break
+            }
+        }
+
+        for char in row.chars().rev() {
+            if char.is_digit(10) {
+                row_res.push(char);
+                break
+            }
+        }
+
+        let row_res_int: i32 = row_res.parse().unwrap();
+
+        res = res + row_res_int;
+    }
+
+    println!("{res}")
 }
