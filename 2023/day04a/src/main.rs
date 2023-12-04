6672a1a55dfc5b927e110a42810bc8e602f9e64d
Tomas Gilys <tomas.gilys@pm.me>
2023-12-04 19:35:49 +0200

    Add day04a solution

---
+++ 2023/day04a/src/main.rs
@@ -0,0 +1,40 @@
+use std::collections::HashSet;
+use aoc_utils::bench;
+use regex::Regex;
+use std::fs;
+
+fn solve() {
+    let file = fs::read_to_string("./input.txt").unwrap();
+
+    let re = Regex::new(r"Card.*: (?<winning>.*) \| (?<owned>.*)").unwrap();
+    let mut result: i32 = 0;
+
+    for row in file.lines() {
+        let mut it = re.captures_iter(row);
+        let caps = it.next().unwrap();
+
+        let winning = &caps["winning"]
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse::<i32>().ok().expect("parse panic"))
+            .collect::<HashSet<_>>();
+
+        let owned = &caps["owned"]
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse::<i32>().ok().expect("parse panic"))
+            .collect::<HashSet<_>>();
+
+        let intersection = winning.intersection(&owned).collect::<Vec<_>>();
+
+        if intersection.len() > 0 {
+            result += i32::pow(2, (intersection.len() - 1) as u32);
+        }
+
+    }
+    println!("{result}")
+}
+
+fn main() {
+    bench(solve)
+}
